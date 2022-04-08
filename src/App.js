972f4ba17dc5b5ef33f7c3589c090fec9de0cc7b
gilkag2 <gilkag2@gmail.com>
2022-04-08 15:40:42 +0300

    adding item to the cart, also updated cart badge for total items

--- src/App.js
+++ src/App.js
@@ -3,21 +3,31 @@ import React, { useEffect, useState } from 'react';
 import { Products, Navbar } from './components';
 const App = () => {
     const [ products, setProducts ] = useState([]);
-
+    const [ cart, setCart ] = useState({});
 
     const fetchProducts = async () => {
+        console.log("fetching");
         const { data } = await commerce.products.list();
         setProducts(data);
     };
+    const fetchCart = async () => {
+        const cart = await commerce.cart.retrieve();
+        setCart(cart);
+    };
 
+    const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity);
+        setCart(item.cart);
+    };
     useEffect(() => {
         fetchProducts();
+        fetchCart();
     }, []);
 
     return (
         <>
-            <Navbar />
-            <Products products={products} />
+            <Navbar totalItems={cart.total_items} />
+            <Products products={products} onAddToCart={handleAddToCart} />
         </>
     );
 };
